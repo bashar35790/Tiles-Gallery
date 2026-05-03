803b7c7a08ca209836f1dcea02f384946a4f321a
Abul Bashar <bashar35790@gmail.com>
2026-05-04 03:23:29 +0600

    fixed environment variable

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.DATABASE_URL!);
+const client = new MongoClient(process.env.DATABASE_URI!);
 const db = client.db("tiles-gallery");
 
 export const auth = betterAuth({
